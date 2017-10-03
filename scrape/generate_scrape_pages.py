58e11a87b8c34a25271c71811fd795ebf99307c1
nlpcall <reynoldsnlp@users.noreply.github.com>
2017-10-03 10:33:07 -0600

    more aesthetic changes to toy scraping files

--- scrape/generate_scrape_pages.py
+++ scrape/generate_scrape_pages.py
@@ -7,12 +7,11 @@ combos = [a + b for a in letters for b in letters]
 
 for c in combos:
     with open(c + '.html', 'w') as h_file:
-        h_file.write('<!DOCTYPE html>\n')
         h_file.write('<html>\n<body>\nHere are the links: <br />\n')
         counter = 1
-        for link in sample(combos, 5):
-            if link != c:  # don't put page's link to itself
-                h_file.write('<a href="https://reynoldsnlp/scrape/' + link +
-                             '.html">Link {}</a> <br />\n'.format(counter))
+        for lnk in sample(combos, 5):
+            if lnk != c:  # don't put page's link to itself
+                h_file.write('<a href="https://reynoldsnlp.com/scrape/' + lnk +
+                             '.html">Link {}</a>\n'.format(counter))
                 counter += 1
         h_file.write('</body>\n</html>\n')

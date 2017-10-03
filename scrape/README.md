01f58f72521792e0079f766b16ed00ea55c94b42
nlpcall <reynoldsnlp@users.noreply.github.com>
2017-10-03 10:40:48 -0600

    last aesthetic changes to toy scraping files

--- scrape/README.md
+++ scrape/README.md
@@ -4,3 +4,7 @@ This folder is for a scraping assignment for my students.
 
 The idea is to build a scraper that will iteratively follow links on all the
 pages, while using a cache to avoid needlessly requesting pages multiple times.
+
+There are 676 html pages. Depending on how the randomly assigned links work
+out, and depending on which page is used as the start page, a crawler may or
+may not be able to find all of them.
--- scrape/generate_scrape_pages.py
+++ scrape/generate_scrape_pages.py
@@ -7,11 +7,12 @@ combos = [a + b for a in letters for b in letters]
 
 for c in combos:
     with open(c + '.html', 'w') as h_file:
+        h_file.write('<!DOCTYPE html>\n')
         h_file.write('<html>\n<body>\nHere are the links: <br />\n')
         counter = 1
-        for lnk in sample(combos, 5):
+        for lnk in sample(combos, 10):
             if lnk != c:  # don't put page's link to itself
                 h_file.write('<a href="https://reynoldsnlp.com/scrape/' + lnk +
-                             '.html">Link {}</a>\n'.format(counter))
+                             '.html">Link {}</a> <br />\n'.format(counter))
                 counter += 1
         h_file.write('</body>\n</html>\n')
